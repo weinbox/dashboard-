94b139dfe786bf54ca2320ed50bfd7fdf28e74b9
weinbox <ahmedigi56@gmail.com>
2026-05-12 10:19:26 +0300

    إزالة المتاجر - المشروع الآن للمواقع العالمية فقط

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import SplashPage from './SplashPage'
-import StorePage from './StorePage'
-import SellerDashboard from './SellerDashboard'
 import AdminPanel from './AdminPanel'
 import ChinaShop from './ChinaShop'
 import ChinaCheckout from './ChinaCheckout'
@@ -10,8 +8,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<SplashPage />} />
-      <Route path="/store/:slug" element={<StorePage />} />
-      <Route path="/seller" element={<SellerDashboard />} />
       <Route path="/china/:provider" element={<ChinaShop />} />
       <Route path="/china-checkout" element={<ChinaCheckout />} />
       <Route path="/ax9admin" element={<AdminPanel />} />
